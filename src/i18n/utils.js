6fd7c71892b32092f6772f2ff9bc63da4a533e57
Roberto J. Vargas <rvargas@rv-solutions.net>
2025-06-09 09:50:16 -0500

    feat: Adding Route es to home

--- src/i18n/utils.js
+++ src/i18n/utils.js
@@ -2,7 +2,11 @@ import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url) {
   const [, lang] = url.pathname.split('/');
+  // Si no hay lang en la URL o es la ruta raíz, devolver el idioma por defecto (español)
+  if (!lang) return defaultLang;
+  // Si el lang está en nuestro objeto ui de traducciones, devolverlo
   if (lang in ui) return lang;
+  // En cualquier otro caso, devolver el idioma por defecto
   return defaultLang;
 }
 
@@ -13,6 +17,11 @@ export function useTranslations(lang) {
 }
 
 export function localizePath(path, lang) {
-  // Si el path empieza con /, vamos a mantenerlo así
+  // Si el idioma es español, no agregamos el prefijo de idioma
+  if (lang === 'es') {
+    return path.startsWith('/') ? path : `/${path}`;
+  }
+  
+  // Para otros idiomas, agregamos el prefijo
   return `/${lang}${path.startsWith('/') ? path : `/${path}`}`;
 }
